34044c58bc1d486e6f6bbf9dcb1e623293bbc7b6
ShadowCat567 <lv2067@nyu.edu>
2021-11-13 11:51:10 -0500

    Weapon Changing

--- Assets/BasicPlayerMove.cs
+++ Assets/BasicPlayerMove.cs
@@ -9,14 +9,16 @@ public class BasicPlayerMove : MonoBehaviour
     [SerializeField] float moveSpeed = 5.0f;
     [SerializeField] float moveX;
 
+    public GameObject swordObj;
+    public GameObject spearObj;
+    public GameObject sheildObj;
+
     enum currentWeapon
     {
-        sword, spear, shield
+        none, sword, spear, shield
     }
 
     [SerializeField] currentWeapon weaponInHand = currentWeapon.sword;
-
-    List<GameObject> weapons = new List<GameObject>();
 
     private void Awake()
     {
@@ -32,10 +34,49 @@ public class BasicPlayerMove : MonoBehaviour
     void Update()
     {
         PlayerControls();
+
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            weaponChanger();
+        }
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveX * moveSpeed, rb.velocity.y);
+    }
+
+    void weaponChanger()
+    {
+        switch(weaponInHand)
+        {
+            case currentWeapon.none:
+                swordObj.SetActive(false);
+                spearObj.SetActive(false);
+                sheildObj.SetActive(false);
+                weaponInHand = currentWeapon.sword;
+                break;
+                    
+            case currentWeapon.sword:
+                swordObj.SetActive(true);
+                spearObj.SetActive(false);
+                sheildObj.SetActive(false);
+                weaponInHand = currentWeapon.spear;
+                break;
+
+            case currentWeapon.spear:
+                swordObj.SetActive(false);
+                spearObj.SetActive(true);
+                sheildObj.SetActive(false);
+                weaponInHand = currentWeapon.shield;
+                break;
+
+            case currentWeapon.shield:
+                swordObj.SetActive(false);
+                spearObj.SetActive(false);
+                sheildObj.SetActive(true);
+                weaponInHand = currentWeapon.none;
+                break;
+        }
     }
 }
