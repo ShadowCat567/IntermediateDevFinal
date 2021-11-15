8ce8d79ec107f2bcb8769b29470c2dcd27f9de3e
LilyYuYXT <xy2033@nyu.edu>
2021-11-15 14:38:35 -0500

    The platform code

--- Assets/Scenes/movement.cs
+++ Assets/Scenes/movement.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float Playerspeed  ;
+    public float Playerspeed;
     public float Jumpforce;
 
-    private Rigidbody2D rb;
+    public Rigidbody2D rb;
     private float originalspeed;
     public float launchforce;
-    
+
     void Start()
     {
         Playerspeed = 5f;
         rb = GetComponent<Rigidbody2D>();
         originalspeed = 5f;
         launchforce = 18;
-        
+
     }
 
     // Update is called once per frame
@@ -27,39 +27,38 @@ public class movement : MonoBehaviour
 
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * Playerspeed;
-        if (Input.GetKeyDown(KeyCode.Space)&&Mathf.Abs(rb.velocity.y)<0.001f)
+        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y) < 0.001f)
         {
             rb.AddForce(new Vector2(0, Jumpforce), ForceMode2D.Impulse);
         }
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.tag=="speedzone")
-        {
-            Debug.Log("fk");
-            Playerspeed =Playerspeed * 3;
-
-        }
-    }
-    void OnCollisionExit2D(Collision2D collision)
-    {
         if (collision.gameObject.tag == "speedzone")
         {
             Debug.Log("fk");
-            Playerspeed = originalspeed;
+            //Playerspeed = Playerspeed * 3;
 
         }
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-      if (collision.gameObject.tag == "bounce")
-            {
-            // Debug.Log("fk");
-            rb.velocity = Vector2.up * launchforce;
-             
-
-            }
-        }
     
+    public void JumpZone()
+    {
+
+
+        rb.velocity = Vector2.up * launchforce;
+
+
+    }
+    public void Speedzone() {
+
+
+        Playerspeed = Playerspeed * 3;
+    }
+    public void Originalspeed()
+    {
+        Playerspeed = originalspeed;
+    }
+
 
 }
