0b074794c71a9bb98f3b3e4caee1a0e478d8c5d8
Kiki <kcb405@nyu.edu>
2021-12-11 12:19:42 -0500

    Enemy Collision

--- Assets/Prefabs/EnemyPatrol.cs
+++ Assets/Prefabs/EnemyPatrol.cs
@@ -15,44 +15,36 @@ public class EnemyPatrol : MonoBehaviour
     public float moveSpeed;
     public bool moveRight; //whether he is moving left or right
 
-    //values for making enemy turn 
-    public Transform wallCheck;
-    public float wallCheckRadius;
-    public LayerMask whatIsWall;
-    private bool hittingWall;
-
-/*    private bool notAtEdge;
-    public Transform edgeCheck;
-*/
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(collisiion2D, collision)
     {
-        //in update
-
-          hittingWall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
-        //        notAtEdge = Physics2D.OverlapCircle(edgeCheck.position, wallCheckRadius, whatIsWall);
-
-        if (hittingWall)
-           moveRight = !moveRight;
-
-        //adds basic enemy movement
-        if (moveRight)
+        if (collision.gameObject.tag == "Wall")
         {
-            transform.localScale = new Vector3(-1f, 1f, 0f); //flips the enemy and wallcheck 
-            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            moveRight = !moveRight;
         }
-        else
-        {
-            transform.localScale = new Vector3(1f, 1f, 0f);
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-        }
-
-        //makes enemy turn around when hitting a wall
     }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            //adds basic enemy movement
+            if (moveRight)
+            {
+                transform.localScale = new Vector3(-1f, 1f, 0f); //flips the enemy and wallcheck 
+                GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            }
+            else
+            {
+                transform.localScale = new Vector3(1f, 1f, 0f);
+                GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            }
+
+            //makes enemy turn around when hitting a wall
+        }
 }
