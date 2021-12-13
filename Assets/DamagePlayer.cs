e51629824c02db1e79a154175960b97ced779843
Kiki <kcb405@nyu.edu>
2021-12-13 03:17:37 -0500

    Enemy Damage Fixed

--- Assets/DamagePlayer.cs
+++ Assets/DamagePlayer.cs
@@ -10,7 +10,7 @@ public class DamagePlayer : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             //      collision.gameObject.GetComponent<>().playerHealth -= 0.1f;
-            collision.gameObject.GetComponent<PlayerHealth>().playerHealth -= 0.1f;
+            collision.gameObject.GetComponent<PlayerHealth>().playerHealth -= 0.25f;
         }
     }
 
